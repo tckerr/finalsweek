c7b97ef3397a4873095d158c61c09503243aedbc
Tom <tckerr@gmail.com>
2017-05-17 07:47:57 -0400

    Random seeding for names and fix for good sttory card

--- finalsweek/util/random.py
+++ finalsweek/util/random.py
@@ -11,7 +11,7 @@ this = sys.modules[__name__]
 def reseed(seed=None):
     import random as __new_random
     this.__seeded_random = __new_random.Random(seed or generation["seed"])
-
+    __names.random = this.__seeded_random
 
 reseed()
 
@@ -22,7 +22,8 @@ def random_id():
 
 
 def random_name():
-    return __names.get_full_name(gender="female")
+    gender = this.__seeded_random.choice("male, female")
+    return __names.get_full_name(gender=gender)
 
 
 def shuffle(lst):
