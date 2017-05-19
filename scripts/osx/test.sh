87c7c25f4fa9b84094fada9be183aa49ce14e8c6
Tom Kerr <tkerr@Toms-MacBook-Pro-2.local>
2017-05-19 17:42:05 -0400

    Fixed osx test script

--- scripts/osx/test.sh
+++ scripts/osx/test.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+
+REPODIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../.. && pwd )"
+source $REPODIR/.venv/bin/activate
+python $REPODIR/finalsweek/manage.py create_single
